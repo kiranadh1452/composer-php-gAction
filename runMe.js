4e99344f46510a749b59611b0ff75866ce4ec734
Kiran Adhikari <kiranadh1452@gmail.com>
2022-06-09 14:20:53 +0545

    test case added

---
+++ runMe.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+
+fs.readFile("test.txt", (err, data) => {
+  if (err) throw err;
+
+  const out = data.toString().trim();
+
+  if (out == "Line no. 1") {
+    fs.appendFile("test.txt", "This is new data added over here", (err) => {
+      if (err) throw err;
+    });
+  }
+});
